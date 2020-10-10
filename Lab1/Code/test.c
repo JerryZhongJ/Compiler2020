dde4bf3526663d2692b79acc45718ed2d79287b7
dreamcode <m15979634309@163.com>
2020-10-10 22:48:12 +0800

    词法分析初次完成，完成基本测试，可能需要更多的测试

---
+++ Lab1/Code/test.c
@@ -0,0 +1,34 @@
+int main()
+{
+    int i = 1;
+    int j = ~i;
+}
+
+int main()
+{
+    float a[10][2];
+    int i;
+    a[5,3] = 1.5;
+    if (a[1][2] == 0)i = 1 else i = 0;
+}
+
+
+int main()
+{
+    int i;
+    i = i+1;
+}
+
+int main()
+{
+    float a = 0.4;
+    a = 0.2e5;
+    a = .3E-5;
+    a = 3.E-6;
+    a = .E5;
+    int b = 1000;
+    b = 070;
+    b = 098;
+    b = 0x1f9;
+    b = 1f8;
+}
