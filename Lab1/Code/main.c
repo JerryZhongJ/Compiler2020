7ddd9aea5e1da1bdaa429f27d8b9bf53286bdb5b
dreamcode <m15979634309@163.com>
2020-10-14 22:45:06 +0800

    error process 1.0 ver

---
+++ Lab1/Code/main.c
@@ -0,0 +1,18 @@
+#include "common.h"
+void yyrestart(FILE *f);
+int main(int argc, char **argv)
+{
+	if (argc <= 1)
+		return 1;
+	FILE *f = fopen(argv[1], "r");
+	if (!f)
+	{
+		perror(argv[1]);
+		return 1;
+	}
+	yyrestart(f);
+	//yydebug = 1;
+	if (!yyparse() && errorNum == 0)
+		display(start, 0);
+	return 0;
+}
